ee84de17522ca6a7320e217600dc084701bfeac7
Sural-mk <surafel.melaku.website@gmail.com>
2024-03-14 21:33:51 +0300

    updated

--- frontend/src/constants/data.js
+++ frontend/src/constants/data.js
@@ -3,27 +3,27 @@
 const wines = [
     {
         title: "Acacia Red Wine",
-        price: '$24',
+        price: '$70',
         tags:"cc | Bottle",
     },
     {
-        title: "Acacia Red Wine",
-        price: '$24',
+        title: "Merlot Red Wine",
+        price: '$150',
         tags:"cc | Bottle",
     },
     {
-        title: "Acacia Red Wine",
-        price: '$24',
+        title: "Chardonnay White Wine",
+        price: '$100',
         tags:"cc | Bottle",
     },
     {
-        title: "Acacia Red Wine",
-        price: '$24',
+        title: "Acacia Rose Wine",
+        price: '$200',
         tags:"cc | Bottle",
     },
     {
-        title: "Acacia Red Wine",
-        price: '$24',
+        title: "Madeira Red Wine",
+        price: '$130',
         tags:"cc | Bottle",
     },
 
@@ -32,28 +32,28 @@ const wines = [
 
 const coktails =[
     {
-        title: "Remedy",
+        title: "Gibson Martini",
         price: "$59",
         tags: "cc | glass | bottle"
     },
     {
-        title: "Remedy",
-        price: "$59",
+        title: "Frozen Margarita",
+        price: "$300",
         tags: "cc | glass | bottle"
     },
     {
-        title: "Remedy",
-        price: "$59",
+        title: "Moscow Mule",
+        price: "$85",
         tags: "cc | glass | bottle"
     },
     {
-        title: "Remedy",
-        price: "$59",
+        title: "Classic Cosmopolitan",
+        price: "$60",
         tags: "cc | glass | bottle"
     },
     {
-        title: "Remedy",
-        price: "$59",
+        title: "Ramos Gin Fizz",
+        price: "$150",
         tags: "cc | glass | bottle"
     }
 ];
