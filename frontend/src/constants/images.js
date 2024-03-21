34ca1ab80a1fe666d2e6db7c81023ef73e072587
Suralmk <surafelmelaku940@gmail.com>
2024-03-21 20:53:52 +0300

    images compressed

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,37 +1,24 @@
 //importing the images and export them
-import logo from '../assets/logo.png'
-import image1 from '../assets/image1.jpg'
-import image2 from '../assets/image2.jpg'
-import image3 from '../assets/image3.jpg'
 import cafe4 from '../assets/cafe4.jpg'
 import cafe1 from '../assets/cafe1.jpg'
-import sura from '../assets/sura.jpg'
 import cafe5 from '../assets/cafe5.jpg'
 import cafe6 from '../assets/cafe6.jpg'
 import cafe7 from '../assets/cafe7.jpg'
 import cafe8 from '../assets/cafe8.jpg'
 import cafe9 from '../assets/cafe9.jpg'
-
 import chef from '../assets/chef.jpg'
-import bg3 from '../assets/bg3.jpg'
-import bg4 from '../assets/bg4.jpg'
+import bg4 from '../assets/bg4.png'
 import pizza from '../assets/pizza.png'
-import animpics from '../assets/anim.png'
-import mainfood from '../assets/mainfood.png'
+import mainfood from '../assets/mainfood.jpeg'
 import coktail from '../assets/cocktail.png'
 import spinner from '../assets/spinner.svg'
 export default{
-    logo,
-    image1,
-    image2,
-    image3,
     cafe4,
     cafe1,
-    sura,
-    cafe5,
+     cafe5,
     cafe6,
     cafe7,
     cafe8,
-    cafe9, chef, bg3, bg4, pizza, animpics, mainfood, coktail, spinner
+    cafe9, chef, bg4, pizza,  mainfood, coktail, spinner
 
 }
