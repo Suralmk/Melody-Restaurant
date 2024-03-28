80a3e98db8657881ba092ae6bcdd7cb213dd122f
Surafel <104755355+Suralmk@users.noreply.github.com>
2024-03-29 02:24:21 +0300

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555
 export const MONGOURL =
-  'mongodb+srv://melody-restaurant:11223344@melody.oqjmm6l.mongodb.net/Melody-reservation?retryWrites=true&w=majority&appName=Melody'
+  'mongodb+srv://melody-restaurant:3344@melody.oqjmm6l.mongodb.net/Melody-reservation?retryWrites=true&w=majority&appName=Melody'
